e48a168cd357ddcdc9a4050a9043a91b14313e86
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-06-06 12:12:49 -0300

    Continuação da localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -47,3 +47,71 @@ const saveTodo = (text, done = 0, save = 1) => {
   
     todoInput.value = "";
   };
+  const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+  };
+  
+  const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+  
+    todos.forEach((todo) => {
+      let todoTitle = todo.querySelector("h3");
+  
+      if (todoTitle.innerText === oldInputValue) {
+        todoTitle.innerText = text;
+  
+        // Utilizando dados da localStorage
+        updateTodoLocalStorage(oldInputValue, text);
+      }
+    });
+  };
+  
+  const getSearchedTodos = (search) => {
+    const todos = document.querySelectorAll(".todo");
+  
+    todos.forEach((todo) => {
+      const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+  
+      todo.style.display = "flex";
+  
+      console.log(todoTitle);
+  
+      if (!todoTitle.includes(search)) {
+        todo.style.display = "none";
+      }
+    });
+  };
+  
+  const filterTodos = (filterValue) => {
+    const todos = document.querySelectorAll(".todo");
+  
+    switch (filterValue) {
+      case "all":
+        todos.forEach((todo) => (todo.style.display = "flex"));
+  
+        break;
+  
+      case "done":
+        todos.forEach((todo) =>
+          todo.classList.contains("done")
+            ? (todo.style.display = "flex")
+            : (todo.style.display = "none")
+        );
+  
+        break;
+  
+      case "todo":
+        todos.forEach((todo) =>
+          !todo.classList.contains("done")
+            ? (todo.style.display = "flex")
+            : (todo.style.display = "none")
+        );
+  
+        break;
+  
+      default:
+        break;
+    }
+  };
